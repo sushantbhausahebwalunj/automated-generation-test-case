fe27ee77248305772788b62008550f40d977bdb6
vaishnavihole <vaishnavihole1@gmail.com>
2023-11-13 17:37:45 +0530

    signup form added

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+
+            <div className='text-footer'>
+                Developed By <h5>Tejaswini Ugale,Tejaswini Khedkar
+                    ,Namita Sarate,Varsha Yadav</h5>
+
+            </div>
+            <div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ client/src/views/Signup/Signup.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import './Signup.css';
+// import Navbar from '../../components/Navbar/Navbar';
+// import Footer from '../../components/Footer/Footer';
+
+const SignUpForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    mobile: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your signup logic here, such as sending data to a server
+    console.log('Form data submitted:', formData);
+  };
+
+  return (
+    <>
+    {/* <Navbar /> */}
+    <div className="signup-form">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="mobile">Mobile:</label>
+          <input
+            type="text"
+            id="mobile"
+            name="mobile"
+            value={formData.mobile}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="button" className='signup-btn'>Sign Up</button>
+      </form>
+    </div>
+    {/* <Footer /> */}
+    </>
+  );
+};
+
+export default SignUpForm;
