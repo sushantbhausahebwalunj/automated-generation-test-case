1463a23bb2dd462d7acb1de4b0660c348b784c4c
vaishnavihole <vaishnavihole1@gmail.com>
2023-11-23 10:06:47 +0530

    signup form integration added

--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -1,86 +1,130 @@
-import React, { useState } from 'react';
-import './Signup.css';
-import Navbar from '../../components/Navbar/Navbar';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./Signup.css";
+import { Link } from "react-router-dom";
 import Footer from '../../components/Footer/Footer';
+import Navbar from '../../components/Navbar/Navbar';
 
-const SignUpForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    mobile: ''
-  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
+function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [mobile, setMobile] = useState("");
+
+  const signup = async () => {
+    if(!name){
+      alert("Name is required");
+      return;
+    }
+
+    if(!email){
+      alert("Email is required");
+      return;
+    }
+
+    if(!password){
+      alert("Password is required");
+      return;
+    }
+
+    if(!mobile){
+      alert("Mobile is required");
+      return;
+    }
+
+    const response = await axios.post("/signup", {
+      name: name,
+      email: email,
+      password: password,
+      mobile: mobile,
+    })
+
+    alert(response?.data?.message);
+
+    if(response?.data?.success){
+      window.location.href = "/login";
+    }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add your signup logic here, such as sending data to a server
-    console.log('Form data submitted:', formData);
-  };
+  useEffect(()=>{
+    const storageUser = JSON.parse(localStorage.getItem("user") || '{}');
+
+    if(storageUser?.email){
+      alert("You are already logged in!");
+      window.location.href = "/";
+    }
+
+  }, [])
 
   return (
-    <>
-    <Navbar />
-    <div className="signup-form">
-      <h2 className='form-title'>Sign Up</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="mobile">Mobile:</label>
-          <input
-            type="text"
-            id="mobile"
-            name="mobile"
-            value={formData.mobile}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="button" className='signup-btn'>Sign Up</button>
-      </form>
-    </div>
-    <Footer />
-    </>
-  );
-};
+    <div>
+      <Navbar />
+      <form className="signup-form">
+        <h1 className='text-center'>Signup</h1>
 
-export default SignUpForm;
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text"
+            placeholder="Enter your name"
+            id="name"
+            className="form-group"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }} />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email"
+            placeholder="Enter your email"
+            id="email"
+            className="form-control"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }} />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password"
+            placeholder="Enter your password"
+            id="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }} />
+        </div>
+
+        <div>
+          <label htmlFor="mobile">Mobile</label>
+          <input type="text"
+            placeholder="Enter your mobile"
+            id="mobile"
+            className="form-control"
+            value={mobile}
+            onChange={(e) => {
+              setMobile(e.target.value);
+            }} />
+        </div>
+
+
+        <button type="button"
+                className="btn signup-btn"
+                onClick={signup}>
+          Signup
+        </button>
+
+        <p className="text-right">
+          <Link to="/login">Already have an account?</Link>
+        </p>
+      </form>
+      <Footer />
+    </div>
+  )
+}
+
+export default Signup
